66321f829c244c0b5ddbea01c4381e44eb2d9ff3
Matthias Behr <mbehr@mcbehr.de>
2025-10-23 15:07:25 +0200

    feat: display support
    First display support.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,6 @@
+// Todos:
+// [] turn display orientation according to inertia sensor
+
 #![no_std]
 #![no_main]
 #![deny(
@@ -6,12 +9,37 @@
     holding buffers for the duration of a data transfer."
 )]
 
-use defmt::info;
+use defmt::{info, warn};
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
-use esp_hal::clock::CpuClock;
-use esp_hal::timer::timg::TimerGroup;
+use embedded_graphics::{
+    geometry::AnchorPoint,
+    mono_font::{MonoTextStyle, ascii::FONT_8X13},
+    pixelcolor::Rgb565,
+    prelude::*,
+    primitives::{PrimitiveStyle, PrimitiveStyleBuilder, StrokeAlignment},
+    text::{Alignment, Text},
+};
+use embedded_hal_bus::spi::ExclusiveDevice;
+use esp_hal::{
+    clock::CpuClock,
+    delay::Delay,
+    gpio::{Level, Output, OutputConfig},
+    spi::{
+        Mode,
+        master::{Config, Spi},
+    },
+    time::Rate,
+    timer::timg::TimerGroup,
+};
 use panic_rtt_target as _;
+
+use mipidsi::{
+    Builder,
+    interface::SpiInterface,
+    models::ST7789,
+    options::{ColorOrder, Orientation},
+};
 
 extern crate alloc;
 
@@ -37,6 +65,86 @@ async fn main(spawner: Spawner) -> ! {
 
     info!("Embassy initialized!");
 
+    // init display (1.47inch capacitive touch LCD display, 172×320 resolution, 262K color with display chip Jadard JD9853 (compatible with ST7789? ) and touch chip AXS5106L )
+    // LCD_CLK GPIO1
+    // LCD_DIN GPIO2
+    // LCD_CS GPIO14
+    // LCD_DC GPIO15
+    // LCD_RST GPIO22
+    // LCD_BL GPIO23
+    let mut lcd_bl = Output::new(peripherals.GPIO23, Level::Low, OutputConfig::default());
+    let lcd_cs = Output::new(peripherals.GPIO14, Level::Low, OutputConfig::default());
+    let lcd_dc = Output::new(peripherals.GPIO15, Level::Low, OutputConfig::default()); // level low according to example from https://github.com/almindor/mipidsi/blob/master/examples/spi-ili9486-esp32-c3/src/main.rs
+    let mut lcd_rst = Output::new(peripherals.GPIO22, Level::Low, OutputConfig::default());
+    lcd_rst.set_high(); // todo why not directly high? (taken from above example)
+
+    // according to spec 16ns is min write cycle duration (<62.5MHz), lets try 40Mhz
+    let display_spi = Spi::new(
+        peripherals.SPI2,
+        Config::default()
+            .with_frequency(Rate::from_mhz(40))
+            .with_mode(Mode::_0),
+    );
+    if display_spi.is_err() {
+        warn!(
+            "Failed to initialize SPI2 for display with error {:?}",
+            display_spi.err()
+        );
+        Timer::after(Duration::from_secs(2)).await;
+        panic!("Failed to initialize SPI2 for display");
+    }
+    
+    let display_spi = display_spi.unwrap();
+    let display_spi = display_spi
+        .with_sck(peripherals.GPIO1)
+        .with_mosi(peripherals.GPIO2);
+
+    let spi_device = ExclusiveDevice::new(display_spi, lcd_cs, embassy_time::Delay).unwrap();
+
+    let mut buffer = [0_u8; 512];
+
+    let di = SpiInterface::new(spi_device, lcd_dc, &mut buffer);
+
+    let mut delay = Delay::new();
+    let mut display = Builder::new(ST7789, di)
+        .display_size(172, 320)
+        .display_offset(34, 0)
+        .color_order(ColorOrder::Bgr)
+        .orientation(Orientation::new().flip_horizontal()) // with this the display is higher than wide, upper left is origin, usb connecctor is on the lower side
+        .reset_pin(lcd_rst)
+        .init(&mut delay)
+        .unwrap();
+    info!(
+        "Display initialized. Size: {}Wx{}H",
+        display.size().width,
+        display.size().height
+    );
+    lcd_bl.set_high(); // turn on backlight
+
+    display.clear(Rgb565::BLACK).unwrap();
+    info!("Display cleared");
+    let border_stroke = PrimitiveStyleBuilder::new()
+        .stroke_color(Rgb565::BLUE)
+        .stroke_width(1)
+        .stroke_alignment(StrokeAlignment::Inside)
+        .build();
+    display
+        .bounding_box()
+        .into_styled(border_stroke)
+        .draw(&mut display)
+        .unwrap();
+
+    let text = "Roonstr.23 - Hzg";
+    let character_style = MonoTextStyle::new(&FONT_8X13 /*FONT_6X10*/, Rgb565::WHITE);
+    Text::with_alignment(
+        text,
+        display.bounding_box().anchor_point(AnchorPoint::TopCenter) + Point::new(0, 15),
+        character_style,
+        Alignment::Center,
+    )
+    .draw(&mut display)
+    .unwrap();
+
     let radio_init = esp_radio::init().expect("Failed to initialize Wi-Fi/BLE controller");
     let (mut _wifi_controller, _interfaces) =
         esp_radio::wifi::new(&radio_init, peripherals.WIFI, Default::default())
@@ -46,11 +154,37 @@ async fn main(spawner: Spawner) -> ! {
     let _ = spawner;
 
     let mut cnt = 0u32;
+    let mut angle_bg = None;
     loop {
-        info!("Hello world #{}", cnt);
-        Timer::after(Duration::from_secs(1)).await;
-        cnt += 1;
-    }
+        //info!("Hello world #{}", cnt);
+        Timer::after(Duration::from_millis(40)).await;
+        cnt += 20; // 1 turn in 2s
 
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-rc.1/examples/src/bin
+        // progress alike circle/arc
+        let angle_start = ((cnt % 360) as f32).deg();
+        //let angle_end = angle_start + 30.0.deg();
+        let angle_sweep = 100.0.deg();
+
+        if let Some(angle_bg) = angle_bg {
+            embedded_graphics::primitives::Arc::new(
+                Point::new(25, 32),
+                40,
+                angle_bg,
+                if angle_bg - angle_start < 0.0.deg() {
+                    angle_bg - angle_start + 360.0.deg()
+                } else {
+                    angle_bg - angle_start
+                },
+            )
+            .into_styled(PrimitiveStyle::with_stroke(Rgb565::BLACK, 2))
+            .draw(&mut display)
+            .unwrap();
+        }
+
+        embedded_graphics::primitives::Arc::new(Point::new(25, 32), 40, angle_start, angle_sweep)
+            .into_styled(PrimitiveStyle::with_stroke(Rgb565::GREEN, 2))
+            .draw(&mut display)
+            .unwrap();
+        angle_bg = Some(angle_start);
+    }
 }
